4277dbfd4835458fc7b13d62548f8dd017548423
ntoiti <fidesluckia36@gmail.com>
2025-07-28 09:58:47 +0300

    added code

---
+++ leet2044.js
@@ -0,0 +1,68 @@
+// Given an integer array nums, find the maximum possible bitwise OR of a subset of nums and return the number of different non-empty subsets with the maximum bitwise OR.
+
+// An array a is a subset of an array b if a can be obtained from b by deleting some (possibly zero) elements of b. Two subsets are considered different if the indices of the elements chosen are different.
+
+// The bitwise OR of an array a is equal to a[0] OR a[1] OR ... OR a[a.length - 1] (0-indexed).
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,1]
+// Output: 2
+// Explanation: The maximum possible bitwise OR of a subset is 3. There are 2 subsets with a bitwise OR of 3:
+// - [3]
+// - [3,1]
+// Example 2:
+
+// Input: nums = [2,2,2]
+// Output: 7
+// Explanation: All non-empty subsets of [2,2,2] have a bitwise OR of 2. There are 23 - 1 = 7 total subsets.
+// Example 3:
+
+// Input: nums = [3,2,1,5]
+// Output: 6
+// Explanation: The maximum possible bitwise OR of a subset is 7. There are 6 subsets with a bitwise OR of 7:
+// - [3,5]
+// - [3,1,5]
+// - [3,2,5]
+// - [3,2,1,5]
+// - [2,5]
+// - [2,1,5]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 16
+// 1 <= nums[i] <= 105
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countMaxOrSubsets = function(nums) {
+    let count=0
+    let maxOr=0
+    const n=nums.length
+
+    function backtrack(i,currOr){
+        if(i===n){
+            if(currOr==maxOr){
+                count++
+            }
+            else if(currOr>maxOr){
+                maxOr=currOr
+                count=1
+            }
+            return
+        }
+        backtrack(i+1,currOr|nums[i])
+        backtrack(i+1,currOr)
+
+    }
+    backtrack(0,0)
+    return count
+   
+    
+};
+console.log(countMaxOrSubsets([3,2,1,5]))
