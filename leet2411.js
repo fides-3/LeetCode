aa123952f95b8dcf7b7f460c22e80cd31358964c
ntoiti <fidesluckia36@gmail.com>
2025-07-29 10:29:54 +0300

    added code

---
+++ leet2411.js
@@ -0,0 +1,17 @@
+const n = nums.length;
+    const pos = new Array(31).fill(-1);
+    const ans = new Array(n);
+    for (let i = n - 1; i >= 0; --i) {
+        let j = i;
+        for (let bit = 0; bit < 31; ++bit) {
+            if (!(nums[i] & (1 << bit))) {
+                if (pos[bit] !== -1) {
+                    j = Math.max(j, pos[bit]);
+                }
+            } else {
+                pos[bit] = i;
+            }
+        }
+        ans[i] = j - i + 1;
+    }
+    return ans;
