d03c3f7187382104133fd8ae40a1fb52e6ddf771
ntoiti <fidesluckia36@gmail.com>
2025-12-12 12:29:24 +0300

    added a sample array

---
+++ leet344arrays.js
@@ -0,0 +1,44 @@
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s[i] is a printable ascii character.
+
+var reverseString=function(s){
+    
+    let start=0;
+    let end=s.length-1;
+
+    while(start<end){
+        let temp=s[start];
+        s[start]=s[end];
+        s[end]=temp;
+        start++;
+        end--;
+    }
+}
+let arr=["h","e","l","l","o"]
+reverseString(arr)
+console.log(arr)
+
+
